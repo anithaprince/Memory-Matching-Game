1f2e2646a373026200c65a25ba5b574a44935527
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-09-25 12:59:25 -0400

    adding project files

--- app.js
+++ app.js
@@ -51,4 +51,63 @@ $(window).scroll(function() {
 	});
 
 
+  let $count = 'X';
+
+  /*******************************************************************/
+  //function to play game
+  const playGame = (event) =>{
+    const $currentVal =$(event.target).text();
+    if($currentVal == " " && $count == 'X')
+    {
+        $(event.target).text($count).css('color','orange');
+        $count = 'O';
+    }
+    else if ($currentVal == " " && $count == 'O')
+    {
+      $(event.target).text($count).css('color','#7AF724');
+      $count = 'X';
+    }
+    //function to check if game won
+    const checkWin =() =>
+    {
+      const $id = $(event.target).attr('id');
+      const $id_before =$id-1;
+      const $idbefore_before = $id_before-1;
+      // const $after =$id+1;
+      // const $after_after = $after+1;
+      console.log($id + $id_before + $idbefore_before);
+
+      let $valCurrent = $('.squares').eq($id).text();
+      const $val1 =$('.squares').eq($id_before).text();
+      const $val2 =$('.squares').eq($idbefore_before).text();
+      // const $val3 =$('.squares').eq($after).text();
+      // const $val4 =$('.squares').eq($after).text();
+
+      //console.log($valCurrent,$val1,$val2);
+
+      if ($id == '2' || $id == '5' || $id == '8')
+      {
+        if ($valCurrent == $val1 && $val1 == $val2 ){
+          console.log($valCurrent + ' Won the Game');
+        }
+      }
+    }
+
+    checkWin();
+  }
+  /*******************************************************************/
+  //function to create the tic tac board
+  const generateSquares = () => {
+  for(let i = 0; i < 9; i++)
+  {
+    const $square = $('<div>').addClass('squares').text(" ").attr('id',i).appendTo('container');
+    const $squareValue = $square.text();
+    $square.on('click', playGame);
+  }
+
+  }
+  /*******************************************************************/
+  generateSquares();
+
+
 });
