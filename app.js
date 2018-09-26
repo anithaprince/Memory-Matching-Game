c3040234b3854f435c84175722d30f5e1b8358cf
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-09-26 15:51:08 -0400

    adding latest work

--- app.js
+++ app.js
@@ -51,78 +51,110 @@ $(window).scroll(function() {
 	});
 
   /*******************************************************************/
+  // This is th efunction to add image into each grid blocks genertaed
 
   const addColor = (count) =>{
+    //Initializing Variables
     let arrGrid = [];
     let count1 = 0;
-
-      for(let i = 0; i < count; i++)
-      {
-        arrGrid.push(i);
-      }
-      console.log(arrGrid);
-      let k=0;
+    let k=0;
+    for(let i = 0; i < count; i++)
+    {
+      arrGrid.push(i); // adding all generated  grids id to an array
+    }
+      //loop to add images to each block
       while(arrGrid.length>0)
       {
-
-        // const red =  Math.floor(Math.random() *  255);
-        // const green =  Math.floor(Math.random() *  255);
-        // const blue =  Math.floor(Math.random() *  255);
-        // const randHex = 'rgb('+red+','+green+','+blue+')';
-
+        //adding same image to two block
         let $num1 = arrGrid[Math.floor(Math.random()*arrGrid.length)];
         let $index1 = arrGrid.indexOf($num1);
-        //$('#'+$num1).css('background-color', randHex);
-        //$('#'+$num1).css('background-color', 'transparent');
         $('#'+$num1).append('<img src="images/grid'+k+'">');
-        $('#'+$num1).val(k);
+        $('#'+$num1).children().val(k);
+        $('#'+$num1).val('notclicked');
+        console.log($('#'+$num1).val());
+        console.log($('#'+$num1).children().val());
         $('#'+$num1).children().css('visibility', 'hidden');
         arrGrid.splice($index1,1);
 
+        //adding same image to two block
         let $num2 = arrGrid[Math.floor(Math.random()*arrGrid.length)];
         let $index2 = arrGrid.indexOf($num2);
-
-        //$('#'+$num2).css('background-color', randHex);
-        //$('#'+$num2).css('background-color', 'transparent');
         $('#'+$num2).append('<img src="images/grid'+k+'">');
-        $('#'+$num2).val(k);
+        $('#'+$num2).children().val(k);
+        $('#'+$num2).val('notclicked');
+        console.log($('#'+$num2).val());
+        console.log($('#'+$num2).children().val());
         $('#'+$num2).children().css('visibility', 'hidden');
         arrGrid.splice($index2,1);
-
-        console.log($num1,$num2);
-        console.log($index1,$index2);
-        console.log(arrGrid);
-        console.log(arrGrid.length);
         k=k+1;
       }
-
-
-
-
 };
-
   /*******************************************************************/
   //function to create the grids
   const generateSquares = (count) => {
-  let k = 0;
-  for(let i = 0; i < count; i++)
-  {
-    const $square = $('<div>').addClass('squares').attr('id',k).appendTo('container');
-    k= k+1;
-  //  $square.css('scale', '1:1')
-  }
-  addColor(count);
 
-  $('.squares').on('click', (event) =>{
-    console.log('clicked');
-    $(event.currentTarget).css('background-image', 'none');
-    console.log($(event.currentTarget).children());
-    $(event.currentTarget).children().eq(0).css('visibility', 'visible');
-    
+    let k = 0;
+    let gridClickCount = 0;
+    let $grid1;
+    let $grid2;
+    let $checkArr =[];
 
-  });
+    const checkWin = ()=>{
+      let $gridVal1 = $checkArr[0].children().eq(0).val();
+      let $gridVal2 = $checkArr[1].children().eq(0).val();
 
-  }
+      if($gridVal1 == $gridVal2)
+      {
+          console.log('Matched');
+          $checkArr[0].children().remove('slow');
+          $checkArr[1].children().remove('slow');
+          $checkArr[0].css('background', 'none');
+          $checkArr[1].css('background', 'none');
+          $checkArr.splice(1,1);
+          $checkArr.splice(0,1);
+
+      }
+      else {
+        $checkArr[0].children().eq(0).hide('slow');
+        $checkArr[1].children().eq(0).hide('slow');
+        $checkArr[0].val('notclicked');
+        $checkArr[1].val('notclicked');
+        $checkArr.splice(1,1);
+        $checkArr.splice(0,1);
+      }
+    }
+
+    for(let i = 0; i < count; i++)
+    {
+      const $square = $('<div>').addClass('squares').attr('id',k).appendTo('container');
+      k= k+1;
+    }
+    addColor(count); // adding images to blocks
+
+    $('.squares').on('click', (event) =>{
+      gridClickCount+=1;
+      $(event.currentTarget).children().eq(0).css('visibility', 'visible');
+        console.log($(event.currentTarget).val());
+      if(gridClickCount == 1 && $(event.currentTarget).val() == 'notclicked')
+      {
+        $grid1 = $(event.currentTarget);
+        $checkArr.push($grid1);
+        $(event.currentTarget).val('clicked');
+        console.log($(event.currentTarget).val());
+      }
+      else if(gridClickCount == 2 && $(event.currentTarget).val() == 'notclicked')
+      {
+        $grid2=$(event.currentTarget);
+        $checkArr.push($grid2);
+        $(event.currentTarget).val('clicked');
+        console.log($(event.currentTarget).val());
+        checkWin();
+        gridClickCount = 0;
+      }
+    })
+
+}
+
   /*******************************************************************/
   $('#easy').on('click', (event)=>{
     generateSquares(12);
